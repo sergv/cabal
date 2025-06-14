06edad5cfb40ededd43befeb905deda172a0e0fc
Sergey Vinokurov <serg.foo@gmail.com>
2025-06-14 22:38:11 +0100

    [DO NOT SUBMIT] flakes

---
+++ flake.nix
@@ -0,0 +1,22 @@
+{
+  inputs = {
+    nixpkgs = {
+      url = "github:nixos/nixpkgs";
+    };
+    flake-utils = {
+      url = "github:numtide/flake-utils";
+    };
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachSystem ["x86_64-linux" "i686-linux"] (system:
+      let pkgs = nixpkgs.legacyPackages.${system};
+      in {
+        devShell = pkgs.mkShell {
+          nativeBuildInputs = [
+            pkgs.groff
+            pkgs.jq
+          ];
+        };
+      });
+}
